031af97cb2b19338080e8d30410e92e5d2e17555
knakaji <knakaji@mac.titech.ac.jp>
2025-02-11 11:59:11 +0900

    250211

---
+++ rw/animation/2d/randomWalk_2dSquareLattice_ani_v2.py
@@ -0,0 +1,55 @@
+# Animation of 2d Random Walk (Square Lattice model)
+# pythonのリストで書いたオリジナルをnp.ndarrayに置き換える試み（250211）
+
+# 計測結果
+# N = 100, M = 5で
+# v1 = 0.190 s
+# v2 = 0.086 s
+
+import numpy as np
+import matplotlib.pyplot as plt
+import animatplot as amp
+import time
+import randomWalk_2dSquareLatticeFunc_v2 as rdwalk
+
+try:
+    N = int(input('Degree of polymerization (default=10): '))
+except ValueError:
+    N = 10
+
+try:
+    M = int(input('Number of repetition (default=5): '))
+except ValueError:
+    M = 5
+
+start_time = time.process_time()                              # 計算にかかる時間を計測
+
+x_array_steps_m, y_array_steps_m, x_front_m, y_front_m, x_end_m, y_end_m = rdwalk.randomWalk_2d_M(N,M)
+
+fig_title = "2-dimensional Random Walk ($N$ = {0}, $M$ = {1})".format(N,M)
+plot_lim = 2*np.sqrt(N)
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, title=fig_title, xlabel='$X$', ylabel='$Y$',
+        xlim=[-plot_lim, plot_lim], ylim=[-plot_lim , plot_lim])
+ax.grid(axis='both', color="gray", lw=0.5)
+
+randomWalk = amp.blocks.Line(x_array_steps_m, y_array_steps_m, ax=ax, ls='-', c='cyan', zorder=1)
+randomWalk_front = amp.blocks.Scatter(x_front_m, y_front_m, ax=ax, marker="x", c='black', zorder=1)
+randomWalk_end = amp.blocks.Scatter(x_end_m, y_end_m, ax=ax, marker="o", c='red', zorder=3)
+
+timeArray = rdwalk.timeArray(N,M)
+timeLine = amp.Timeline(timeArray, units=' steps', fps=10)
+
+anim = amp.Animation([randomWalk,randomWalk_front,randomWalk_end], timeLine)
+anim.controls()
+
+end_time = time.process_time()                              # 計算にかかる時間を計測
+elapsed_time = end_time - start_time                        # 計算にかかる時間を計測
+print("elapsed time = {0:.3f} sec".format(elapsed_time))    # 計算にかかる時間を表示
+
+savefile = "./gif/randomWalk_2d_N{0}_M{1}".format(N,M)
+anim.save_gif(savefile)
+
+plt.show()
+plt.close()
