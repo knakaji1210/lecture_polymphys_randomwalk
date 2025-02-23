8e2ce60c77ae7a07b48d9bb4591ed42524c2877d
knakaji <knakaji@mac.titech.ac.jp>
2025-02-23 19:47:39 +0900

    SAW with animatplot

---
+++ saw/animation/2d/saw2dFunc_square_v2.py
@@ -0,0 +1,120 @@
+# Function to draw animation of 2d Self-Avoiding Walk (Square Lattice model)
+# matplotlibのanimationからanimatplotに置き換える試み（250223）
+
+import random as rd
+from math import *
+import numpy as np
+
+def saw2dS(N):
+
+    num_step = 0
+
+    theta_s = np.array([0,0.5*np.pi, np.pi, 1.5*np.pi])   # x軸からの角度
+
+    x_list_steps = []
+    y_list_steps = []
+    x_front = []
+    y_front = []
+
+    while num_step < N:
+        num_step = 0
+        num_rep = 0         # 袋小路に入ったときに繰り返す試行回数
+        x, y = 0, 0
+        x_list = [0]
+        y_list = [0]
+        coordinate_list = [[0,0]]   # 通過した点の記録
+        num_step_list = []
+
+        while num_rep < 20 and num_step < N:        # 試行は20回までで諦める
+            theta = np.random.choice(theta_s)
+            x_temp = x + np.rint(np.cos(theta))     # 一致不一致をチェックするため、整数化を行う
+            y_temp = y + np.rint(np.sin(theta))
+            coordinate_temp = [x_temp, y_temp]      # まず仮の新座標を設定
+            if coordinate_temp in coordinate_list:  # もし新座標が既に占有されていたらという条件
+                x = x
+                y = y
+                x_list.append(x) #試行中でステイしていることを示す部分
+                y_list.append(y)
+                num_step = num_step
+                num_step_list.append(num_step)
+                num_rep = num_step_list.count(num_step_list[-1])    # .countメソッドで試行回数を計算
+#                print("failure: num = {0}".format(num_step)) 
+            else:                                   # もし新座標が非占有だったらという条件
+                x = x + np.rint(np.cos(theta))
+                y = y + np.rint(np.sin(theta))
+                x_list.append(x)
+                y_list.append(y)
+                coordinate = [x, y]
+                coordinate_list.append(coordinate)  # 新座標を登録
+                num_step += 1
+#                print("success: num = {0}".format(num_step))
+
+        print("final num = {0}".format(num_step)) # to check the number of steps
+
+        len_list = len(x_list)  # 途中失敗などがあるため、x_listの長さが不定なので計測
+        x_list_step = [ x_list[:i+1] for i in range(len_list) ] # x_listをamp.blocks.Scatterで使える形への変換
+        y_list_step = [ y_list[:i+1] for i in range(len_list) ] # num_stepがNに満たない場合も含める
+        x_front_s = [ [x_list[i]] for i in range(len_list) ]    # x_listをamp.blocks.Scatterで使える形への変換
+        y_front_s = [ [y_list[i]] for i in range(len_list) ]
+        x_list_steps += x_list_step                             # 失敗時も成功時も追加、これで全ての途中過程を追いかけている
+        y_list_steps += y_list_step
+        x_front += x_front_s
+        y_front += y_front_s
+
+    return x_front, y_front, x_list_steps, y_list_steps
+
+def saw2dM(N,M):
+
+    nan = np.nan
+
+    delay_step = 10
+
+    x_list_steps_m = []     # 座標全体
+    y_list_steps_m = []
+    x_front_m = []          # 各ステップの最後の座標
+    y_front_m = []
+    x_end_m = []            # 各トライアルの最後の座標
+    y_end_m = []
+    num_step_list = []      # 各トライアルのステップ数
+
+    for m in range(M):
+        x_front, y_front, x_list_steps, y_list_steps = saw2dS(N)
+        x_list_steps_m += x_list_steps
+        y_list_steps_m += y_list_steps
+        num_step = len(x_front)                 # x_frontの長さが不定のため計測
+        print("l =",num_step)
+        num_step_list.append(num_step)
+        nan_list = [ [nan] for i in range(num_step-1)]
+        x_end = nan_list + [x_front[-1]]        # x_frontの最後をnanにする前に抽出し"o"の座標を作成
+        y_end = nan_list + [y_front[-1]]
+        x_end_m += x_end
+        y_end_m += y_end         
+        x_front[-1] = [nan]                     # x_frontの最後を"x"ではなく"o"にするための変換
+        y_front[-1] = [nan]
+        x_front_m += x_front
+        y_front_m += y_front
+
+        for i in range(delay_step):     # "o"を表示している時間を伸ばすため
+            x_list_steps_m.append(x_list_steps_m[-1])
+            y_list_steps_m.append(y_list_steps_m[-1])
+            x_front_m.append(x_front_m[-1])
+            y_front_m.append(y_front_m[-1])
+            x_end_m.append(x_end_m[-1])
+            y_end_m.append(y_end_m[-1])
+
+        print("repeat num = {0}".format(m+1))
+    
+    return x_list_steps_m, y_list_steps_m, x_front_m, y_front_m, x_end_m, y_end_m, num_step_list
+
+def timeList(num_step_list,M):
+
+    delay_step = 10
+
+    timeList = []
+
+    for m in range(M):
+        num_step = num_step_list[m]
+        t = np.linspace(0, num_step+delay_step-1, num_step+delay_step).tolist()
+        timeList += t
+
+    return timeList
---
+++ saw/animation/2d/selfAvoidWalk_2d_square_ani_v2.py
@@ -0,0 +1,50 @@
+# Animation of 2d Self-Avoiding Walk (Square Lattice model)
+# matplotlibのanimationからanimatplotに置き換える試み（250223）
+
+import numpy as np
+import saw2dFunc_square_v2 as saw
+import matplotlib.pyplot as plt
+import animatplot as amp
+import time
+
+try:
+    N = int(input('Degree of polymerization (default=100): '))
+except ValueError:
+    N = 100
+
+try:
+    M = int(input('Number of repetition (default=3): '))
+except ValueError:
+    M = 3
+
+start_time = time.process_time()                              # 計算にかかる時間を計測
+
+x_list_steps_m, y_list_steps_m, x_front_m, y_front_m, x_end_m, y_end_m, num_step_list = saw.saw2dM(N,M)
+
+fig_title = "2-dimensional Self-Avoiding Walk ($N$ = {0}, $M$ = {1})".format(N,M)
+plot_lim = 4*np.sqrt(N)
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, title=fig_title, xlabel='$X$', ylabel='$Y$',
+        xlim=[-plot_lim, plot_lim], ylim=[-plot_lim , plot_lim])
+ax.grid(axis='both', color="gray", lw=0.5)
+
+randomWalk = amp.blocks.Line(x_list_steps_m, y_list_steps_m, ax=ax, ls='-', c='cyan', zorder=1)
+randomWalk_front = amp.blocks.Scatter(x_front_m, y_front_m, ax=ax, marker="x", c='black', zorder=1)
+randomWalk_end = amp.blocks.Scatter(x_end_m, y_end_m, ax=ax, marker="o", c='red', zorder=3)
+
+timeList = saw.timeList(num_step_list,M)
+timeLine = amp.Timeline(timeList, units=' steps', fps=10)
+
+anim = amp.Animation([randomWalk,randomWalk_front,randomWalk_end], timeLine)
+anim.controls()
+
+end_time = time.process_time()                              # 計算にかかる時間を計測
+elapsed_time = end_time - start_time                        # 計算にかかる時間を計測
+print("elapsed time = {0:.3f} sec".format(elapsed_time))    # 計算にかかる時間を表示
+
+savefile = "./gif/selfAvoidWalk_2d_N{0}_M{1}".format(N,M)
+anim.save_gif(savefile)
+
+plt.show()
+plt.close()
