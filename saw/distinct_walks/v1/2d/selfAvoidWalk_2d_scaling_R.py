882e478bcb77bea027a2809ae48b92d075ce35c4
knakaji <knakaji@mac.titech.ac.jp>
2025-03-02 13:38:38 +0900

    distinct_walks_2d_v1

---
+++ saw/distinct_walks/v1/2d/selfAvoidWalk_2d_scaling_R.py
@@ -0,0 +1,48 @@
+# Scaling of end-to-end distance for 2d Self-Avoiding Walk 
+
+import numpy as np
+from math import *
+import saw2dFuncS3 as saws
+import saw2dFuncM3 as sawm
+import matplotlib.pyplot as plt
+
+N = 5
+trials = 30
+
+walks_list = [[[0, 0]]]
+num = 0
+num_list = []
+R_list = []
+
+for n in range(N):
+    num = num + 1
+    num_list.append(num)
+    walks_list = saws.saw2dDistinctWalks(walks_list, trials)
+    result_list = sawm.saw2dCalcR(walks_list)
+    R = result_list[1]
+    R_list.append(R)
+
+log_n_list = [ log10(x) for x in num_list ]
+log_R_list = [ log10(x) for x in R_list ]
+
+# Least-squares fitting
+
+fit_result = np.polyfit(log_n_list, log_R_list, 1)
+exponent = fit_result[0]
+intercept = fit_result[1]
+fit_func = np.poly1d(fit_result)(log_n_list)
+
+fig = plt.figure()
+
+fig_title = "Scaling of R for 2-d Self-Avoiding Walk"
+result_text = "Sqrt(<R2>) ~ N^("+str(round(exponent, 3))+")"
+
+ax = fig.add_subplot(111,title=fig_title, xlabel='Log(N)', ylabel='Log(R)')
+ax.grid(axis='both', color="gray", lw=0.5)
+ax.scatter(log_n_list, log_R_list)
+ax.plot(log_n_list, fit_func, color="black")
+fig.text(0.5, 0.20, result_text)
+
+fig.savefig("./png/selfAvoidWalk_2d_scaling_R.png", dpi=300)
+
+plt.show()
