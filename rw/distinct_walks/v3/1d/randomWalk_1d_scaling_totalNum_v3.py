94c9ad1fa715b31645fd75b74b5b623097d00c94
knakaji <knakaji@mac.titech.ac.jp>
2025-03-06 22:42:07 +0900

    distinct_walks_1d_v3

--- rw/distinct_walks/v3/1d/randomWalk_1d_scaling_totalNum_v3.py
+++ rw/distinct_walks/v3/1d/randomWalk_1d_scaling_totalNum_v3.py
@@ -44,7 +44,7 @@ fig_title = "Number of Distinct Walks for 1-d Random Walk"
 result_text1 = "$n_{{tot}}$ = ({0:.2f}±{1:.2f})$^N$".format(10**exponent, exponent_error)
 result_text2 = "$N_{{max}}$ = {0}, $T_{{comp}}$ = {1:.2f} s".format(N, elapsed_time)
 
-ax = fig.add_subplot(111,title=fig_title, xlabel='$N$', ylabel='Log($n_{{tot}}$)')
+ax = fig.add_subplot(111,title=fig_title, xlabel='$N$', ylabel='Log($n_{{tot}}$)', xlim=(0, 1.05*N))
 ax.grid(axis='both', color="gray", lw=0.5)
 ax.scatter(num_list, log_list)
 ax.plot(num_list, fit_func, color="black")
--- rw/distinct_walks/v3/1d/rw1dFuncM_v3.py
+++ rw/distinct_walks/v3/1d/rw1dFuncM_v3.py
@@ -39,53 +39,5 @@ def rw1dCalcR(file_num):
     r2_list = [ x**2 + y**2 for x, y in zip(x_list, y_list) ]
     r2_mean = np.mean(r2_list)
     R = np.sqrt(r2_mean)
-    print(R)
-    
 
-    return x_list, y_list, r2_list
-
-
-    #numDistinctWalks = len(walks_list)
-
-    #r2_list = []
-
-    #for i in range(numDistinctWalks):
-    #        coordinate_list = walks_list[i]
-#            print(coordinate_list)
-    #        coordinate_list_end = coordinate_list[-1]
-    #        r2 = coordinate_list_end[0]*coordinate_list_end[0] + coordinate_list_end[1]*coordinate_list_end[1]
-    #        r2_list.append(r2)
-    #else:
-    #    pass
-
-#    r2_mean = np.mean(r2_list)
-#    r2_std = np.std(r2_list)
-#    R = np.sqrt(r2_mean)
-
-#    result_list = [r2_list, R]
-
-#    return result_list
-
-
-def rw1dCalcR_orig(walks_list):
-
-    numDistinctWalks = len(walks_list)
-
-    r2_list = []
-
-    for i in range(numDistinctWalks):
-            coordinate_list = walks_list[i]
-#            print(coordinate_list)
-            coordinate_list_end = coordinate_list[-1]
-            r2 = coordinate_list_end[0]*coordinate_list_end[0] + coordinate_list_end[1]*coordinate_list_end[1]
-            r2_list.append(r2)
-    else:
-        pass
-
-    r2_mean = np.mean(r2_list)
-    r2_std = np.std(r2_list)
-    R = np.sqrt(r2_mean)
-
-    result_list = [r2_list, R]
-
-    return result_list
+    return R
