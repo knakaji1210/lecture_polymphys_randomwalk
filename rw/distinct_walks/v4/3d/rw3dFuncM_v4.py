10bb29db9e2966e9814f4d19ebffa225ced3cc93
knakaji <knakaji@mac.titech.ac.jp>
2025-03-08 17:09:33 +0900

    distinct_walks_3d_v4

---
+++ rw/distinct_walks/v4/3d/rw3dFuncM_v4.py
@@ -0,0 +1,35 @@
+#calculation of statistics of 3d random walk
+
+import numpy as np
+import csv
+
+def rw3dCountDistinctWalks(fileNum):    
+    import_file_x = "./data/distinctWalks_3d_N{0}_x.txt".format(fileNum)   # 読み込むtxtファイルの名前（1次元だけ読めば十分）
+    import_array = np.loadtxt(import_file_x, dtype=int)                    # txtファイルを読み込む
+    numWalks = import_array.shape[0]                                       # そこに含まれるwalkの数を数える
+    return numWalks
+
+def rw3dCalcR(fileNum):
+    numWalks = rw3dCountDistinctWalks(fileNum)
+    x_list = []
+    y_list = []
+    z_list = []
+    import_file_x = "./data/distinctWalks_3d_N{0}_x.txt".format(fileNum)   # 読み込むcsvファイルの名前
+    import_file_y = "./data/distinctWalks_3d_N{0}_y.txt".format(fileNum)
+    import_file_z = "./data/distinctWalks_3d_N{0}_z.txt".format(fileNum)
+    for i in range(numWalks):                                  # 数えたwalkの数だけ繰り返す
+        import_array_x = np.loadtxt(import_file_x, dtype=int, skiprows=i, max_rows=1)
+        import_array_y = np.loadtxt(import_file_y, dtype=int, skiprows=i, max_rows=1)
+        import_array_z = np.loadtxt(import_file_z, dtype=int, skiprows=i, max_rows=1)
+        x_end = import_array_x[-1]
+        y_end = import_array_y[-1]
+        z_end = import_array_z[-1]
+        x_list.append(x_end)
+        y_list.append(y_end)
+        z_list.append(y_end)
+                       
+    r2_list = [ x**2 + y**2 + z**2 for x, y, z in zip(x_list, y_list, z_list) ]
+    r2_mean = np.mean(r2_list)
+    R = np.sqrt(r2_mean)
+
+    return R
---
+++ rw/distinct_walks/v4/3d/rw3dFuncS_v4.py
@@ -0,0 +1,63 @@
+# Functions to generate distinct 3d Random Walk
+# np.savetxtでtxt書き出しを行うバージョン、完全に書き換え（250307）
+# distinctWalks_1d_N1_x.txtとdistinctWalks_1d_N1_y.txtを用意する必要がある（N0をスタートにすると動かない）
+# csvを使ったv3よりも圧倒的に速い
+
+import numpy as np
+
+def rw3dDistinctWalks(N):                                      # Degree of polymerizationが唯一の変数
+
+# Function to obtain distinct 3d Random Walks of (n+1) steps from n steps
+
+    fileNum = N                                                # 何番目のtxtファイルを読み込むか
+
+    direction = np.array([[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]])
+    numDirections = direction.shape[0]                                 # 可能な移動ステップの数
+
+    import_file_x = "./data/distinctWalks_3d_N{0}_x.txt".format(fileNum)    # 読み込むcsvファイルの名前  
+    import_file_y = "./data/distinctWalks_3d_N{0}_y.txt".format(fileNum)
+    import_file_z = "./data/distinctWalks_3d_N{0}_z.txt".format(fileNum)
+    export_file_x = "./data/distinctWalks_3d_N{0}_x.txt".format(fileNum+1)  # 次のステップのcsvファイルの名前
+    export_file_y = "./data/distinctWalks_3d_N{0}_y.txt".format(fileNum+1)
+    export_file_z = "./data/distinctWalks_3d_N{0}_z.txt".format(fileNum+1)
+
+    import_array = np.loadtxt(import_file_x, dtype=int)
+    
+    numWalks = import_array.shape[0]
+    numSteps = import_array.shape[1]
+
+    print('N = {0}, numWalks = '.format(fileNum), numWalks)
+
+    export_array_x = []
+    export_array_y = []
+    export_array_z = []
+
+    for i in range(numWalks):                                  # 数えたwalkの数だけ繰り返す
+        import_array_x = np.loadtxt(import_file_x, dtype=int, skiprows=i, max_rows=1)
+        x_end = import_array_x[-1]
+        import_array_y = np.loadtxt(import_file_y, dtype=int, skiprows=i, max_rows=1)
+        y_end = import_array_y[-1]
+        import_array_z = np.loadtxt(import_file_z, dtype=int, skiprows=i, max_rows=1)
+        z_end = import_array_z[-1]
+        for n in range(numDirections):                      # 可能な移動ステップの数だけ繰り返す
+            step = direction[n]                    # num_stepsで選ばれた移動ステップを選ぶ
+            x = x_end + step[0]                         # 新x座標を作成
+            y = y_end + step[1]                         # 新y座標を作成
+            z = z_end + step[2]                         # 新z座標を作成
+            updated_x_array = np.append(import_array_x, x)     # 新x座標を追加
+            updated_y_array = np.append(import_array_y, y)
+            updated_z_array = np.append(import_array_z, z)
+            export_array_x = np.append(export_array_x, updated_x_array)
+            export_array_y = np.append(export_array_y, updated_y_array)
+            export_array_z = np.append(export_array_z, updated_z_array)
+    
+    export_array_x.resize(numWalks*numDirections,numSteps+1)
+    export_array_y.resize(numWalks*numDirections,numSteps+1)
+    export_array_z.resize(numWalks*numDirections,numSteps+1)
+    np.savetxt(export_file_x, export_array_x, fmt ='%.0f')
+    np.savetxt(export_file_y, export_array_y, fmt ='%.0f')
+    np.savetxt(export_file_z, export_array_z, fmt ='%.0f')
+    numWalks_Last = export_array_x.shape[0]
+
+    return numWalks_Last
+
